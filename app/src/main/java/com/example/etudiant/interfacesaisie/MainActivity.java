22a28934e88d0a5f854d6a325c232cfd310ca327
Alexis <alexis.l@outlook.com>
2018-01-17 16:32:22 +0100

    Changement des couleurs

--- app/src/main/java/com/example/etudiant/interfacesaisie/MainActivity.java
+++ app/src/main/java/com/example/etudiant/interfacesaisie/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.util.Base64;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
